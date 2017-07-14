94bbc84c9e30d8bf04bbd78e4273e883bd5d258b
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-14 11:24:41 +0300

    used singleton method + exec. method to run SQL in the project

--- lib/client.rb
+++ lib/client.rb
@@ -5,4 +5,16 @@ class Client
       @description = attributes.fetch(:description)
       @stylist_id = attributes.fetch(:stylist_id)
     end
+
+
+    define_singleton_method(:all) do
+      returned_clients = DB.exec("SELECT * FROM clients;")
+      clients = []
+      returned_clients.each() do |client|
+        description = client.fetch("description")
+        stylist_id = client.fetch("stylist_id").to_i() # The information comes out of the database as a string.
+        clients.push(Client.new({:description => description, :stylist_id => stylist_id}))
+      end
+      clients
+    end
 end
