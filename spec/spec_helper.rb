8b19a38c4b22c483ea46637f459297e42e1aeac8
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-14 11:10:10 +0300

    used delete method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,10 @@ require("rspec")
 
 
   DB = PG.connect({:dbname => "hair_salon_test"})
+
+  RSpec.configure do |config|
+    config.after(:each) do
+      DB.exec("DELETE FROM stylists *;")
+      DB.exec("DELETE FROM clients *;")
+    end
+  end
