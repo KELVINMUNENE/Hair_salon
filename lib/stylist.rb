a8408e500d48b004915c1bb054b5a0061a5b021f
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-07-14 11:39:16 +0300

    used update and find method to execute changes in saved clients names

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -5,6 +5,7 @@ class Stylist
       @name = attributes.fetch(:name)
       @id = attributes.fetch(:id)
     end
+
     define_singleton_method(:all) do
       returned_stylists = DB.exec("SELECT * FROM stylists;")
       stylists = []
@@ -17,12 +18,43 @@ class Stylist
     end
 
     define_method(:save) do
-  result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
-  @id = result.first().fetch("id").to_i()
-end
+      result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+      @id = result.first().fetch("id").to_i()
+    end
 
-define_method(:==) do |another_stylist|
-  self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
-end
+    define_method(:==) do |another_stylist|
+      self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
+    end
 
+    define_singleton_method(:find) do |id|
+      found_stylist = nil
+      Stylist.all().each() do |stylist|
+        if stylist.id().==(id)
+          found_stylist = stylist
+        end
+      end
+      found_stylist
+    end
+
+    define_method(:clients) do
+      stylist_clients = []
+      clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()};")
+      clients.each() do |client|
+        description = client.fetch("description")
+        stylist_id = client.fetch("stylist_id").to_i()
+        stylist_clients.push(Client.new({:description => description, :stylist_id => stylist_id}))
+      end
+      stylist_clients
+    end
+
+    define_method(:update) do |attributes|
+      @name = attributes.fetch(:name)
+      @id = self.id()
+      DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{@id};")
+    end
+
+    define_method(:delete) do
+      DB.exec("DELETE FROM stylists WHERE id = #{self.id()};")
+      DB.exec("DELETE FROM clients WHERE stylist_id = #{self.id()};")
+    end
 end
